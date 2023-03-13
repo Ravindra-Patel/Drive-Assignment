b1defdf34e818e1950e5fe6b3ee452490e85e8ac
Ravindra Patel <ravindra.patel@bluepineapple.io>
2023-03-13 10:24:13 +0530

    Displaying 4 car card components on x-large screens

--- pages/cars-for-sale/index.js
+++ pages/cars-for-sale/index.js
@@ -4,25 +4,33 @@ import Car from "../../components/Car";
 import HeadMeta from "../../components/HeadMeta";
 
 const CarsForSale = ({ cars }) => {
-    
+  const loadMore = () => {};
+
   return (
     <>
       <HeadMeta title="Cars For Sale" />
-      <div className="min-h-[77vh] flex item-center justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="min-h-[77vh] items-center justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {cars.map((car) => (
             <div key={car.id} className="m-auto md:m-2 w-full">
               <Car car={car} />
             </div>
           ))}
         </div>
+        {/* <div class="flex justify-between">
+          <button
+            class="bg-blue-500 mx-auto my-5 sm:text-sm sm:py-0 h-10 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded "
+            onClick={loadMore}
+          >
+            Load More
+          </button>
+        </div> */}
       </div>
     </>
   );
 };
 
 export default CarsForSale;
-
 
 export async function getStaticProps() {
   const { data } = await client.query({
