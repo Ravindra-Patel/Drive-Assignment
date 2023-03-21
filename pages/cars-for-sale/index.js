0d27699336b9e773853509e6d794ca696f4af7c1
Ravindra Patel <ravindra.patel@bluepineapple.io>
2023-03-21 15:34:22 +0530

    Added client side pagination using load more button

--- pages/cars-for-sale/index.js
+++ pages/cars-for-sale/index.js
@@ -3,29 +3,38 @@ import client from "../../apollo/apollo-client";
 import Car from "../../components/Car";
 import HeadMeta from "../../components/HeadMeta";
 import { GET_CARS } from "../../queries/getAllCars";
+import { useState } from "react";
 
 const CarsForSale = ({ cars }) => {
-  const loadMore = () => {};
+  let totalCars = cars?.length;
+
+  const [showCars, setShowCars] = useState(12);
+
+  const loadMore = () => {
+    setShowCars((prevState) => prevState + 12);
+  };
 
   return (
     <>
       <HeadMeta title="Cars For Sale" />
-      <div className="min-h-[77vh] items-center justify-center">
+      <div className="min-h-[82vh] items-center justify-center">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {cars.map((car) => (
-            <div key={car.id} className="m-auto md:m-2 w-full">
+          {cars.slice(0,showCars).map((car) => (
+            <div key={car.id}>
               <Car car={car} />
             </div>
           ))}
         </div>
-        {/* <div class="flex justify-between">
-          <button
-            class="bg-blue-500 mx-auto my-5 sm:text-sm sm:py-0 h-10 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded "
-            onClick={loadMore}
-          >
-            Load More
-          </button>
-        </div> */}
+        {totalCars > showCars && (
+          <div class="flex justify-between">
+            <button
+              class="bg-blue-500 mx-auto my-5 sm:text-sm sm:py-0 h-10 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded "
+              onClick={loadMore}
+            >
+              Load More
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
