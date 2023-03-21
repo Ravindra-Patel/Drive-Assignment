e7f420ad59704ee2095100b18d21e4a25318f22b
Ravindra Patel <ravindra.patel@bluepineapple.io>
2023-03-21 15:33:20 +0530

    Cleaning the code

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 export default function Home() {
   return (
-    <div className="w-full min-h-[77vh] flex items-center justify-center p-4">
+    <div className="w-full min-h-[82vh] flex items-center justify-center p-4">
       <h1 className="text-xl md:text-3xl font-bold text-justify">
         Welcome! Go to Cars for Sale Page to Purcahse Cars
       </h1>
